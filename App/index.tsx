80964b120bf1be8f1d6e778356a291682fff311e
HungNguyen <hungnq@amtechcom.vn>
2018-09-09 15:12:40 +0700

    config typescripts

--- App/index.tsx
+++ App/index.tsx
@@ -1,23 +1,16 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import * as React from 'react'
+import { Platform, StyleSheet, Text, View } from 'react-native'
 
 const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  ios: 'Press Cmd+R to reload,\n Cmd+D or shake for dev menu',
   android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+    'Double tap R on your keyboard to reload,\n Shake or press menu button for dev menu',
+})
 
-type Props = {};
-export default class App extends Component<Props> {
+export interface Props {}
+export interface State {}
+
+export class App extends React.Component<Props, State> {
   render() {
     return (
       <View style={styles.container}>
@@ -25,7 +18,7 @@ export default class App extends Component<Props> {
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
       </View>
-    );
+    )
   }
 }
 
@@ -46,4 +39,4 @@ const styles = StyleSheet.create({
     color: '#333333',
     marginBottom: 5,
   },
-});
+})
